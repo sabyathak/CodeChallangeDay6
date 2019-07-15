2cec6d994f7a24251af70adc5ae615a6a30345aa
sabyathak <sabyathak22@gmail.com>
2019-07-15 13:22:02 -0700

    Inital Commit

--- CodeChallangeDay6/ViewController.swift
+++ CodeChallangeDay6/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var textBox: UITextField!
+    
+
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+        @IBAction func submitButtonTapped(_ sender: Any) {
+        
+        let newTitle = textBox.text
+            if newTitle == "Scholar 123"
+            {
+            label.text = "Welcome, you have logged in."
+            } else {
+            label.text = "You have entered incorrect information"
+        }
 
+
+}
 
 }
 
